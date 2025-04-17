5bd6310f43937908e924abf75273dcb9213800d8
mauricioboninno <mauribonino5@gmail.com>
2025-04-17 17:48:30 -0300

    Fixes

--- js/footer.js
+++ js/footer.js
@@ -18,13 +18,8 @@
 
   function updateFooterType() {
     element.classList.replace("animate__fadeIn", "animate__fadeOut");
-
-    const defaultText = `
-    made with <span>&lt;3</span> by 
-    <a href="mailto:mauricio@boninno.com.ar">@mau</a> 
-    in 3 minutes`;
     
-    //const defaultText = `made with <span>&lt;3</span> by <a href="mailto:mauricio@boninno.com.ar">@mau</a> in 3 minutes`;
+    const defaultText = `made with <span>&lt;3</span> by <a href="mailto:mauricio@boninno.com.ar">@mau</a> in 3 minutes`;
     const dateTimeText = loadDateTime();
   
     setTimeout(() => {
