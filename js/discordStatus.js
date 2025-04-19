a26c34a86f828213636b0f4605b05e979d696323
mauricioboninno <mauribonino5@gmail.com>
2025-04-19 02:18:32 -0300

    Removed hover menu

--- js/discordStatus.js
+++ js/discordStatus.js
@@ -1,8 +1,5 @@
   const statusDot = document.getElementById('status-dot');
   const statusText = document.getElementById('status-text');
-  const profileImage = document.getElementById('profile-image');
-  const profileUsername = document.getElementById('profile-username');
-  const profileActivity = document.getElementById('profile-activity');
 
   const statusMap = {
     online: "I'm currently online",
@@ -21,10 +18,6 @@
   
       statusDot.className = `dot ${status}`;
       statusText.textContent = statusMap[status] || "Unknown status";
-
-      profileImage.src = `https://cdn.discordapp.com/avatars/${userId}/${data.discord_user.avatar}.png`;
-      profileUsername.textContent = data.discord_user.username;
-      profileActivity.textContent = data.listening_to_music ? `Listening to ${data.listening_to_music.name}` : "No activity";
     } catch (error) {
       statusText.textContent = "Failed to fetch status";
     }
