bdca4b77ff08d27ab926d79be140d138500bd60a
mauricioboninno <mauribonino5@gmail.com>
2025-04-20 04:48:52 -0300

    Test

--- js/main.js
+++ js/main.js
@@ -3,3 +3,11 @@
 
   handleQuotesTyping();
   handleFooterCycling();
+
+  const menuButton = document.getElementById('menu-toggle');
+  const sideMenu = document.getElementById('side-menu');
+
+  menuButton.addEventListener('click', () => {
+    const isOpen = sideMenu.classList.toggle('open');
+    menuButton.innerHTML = isOpen ? '&lt;' : '&gt;';
+  });
