30a814c7255411c3e0400719aac689f0bedd5fbc
mauricioboninno <mauribonino5@gmail.com>
2025-04-19 23:55:14 -0300

    Test

--- js/discordStatus.js
+++ js/discordStatus.js
@@ -1,49 +1,41 @@
-  const statusDot = document.getElementById('status-dot');
-  const statusText = document.getElementById('status-text');
+const statusText = document.getElementById('status-text');
 
-  const statusMap = {
-    online: "I'm currently online",
-    idle: "Idle",
-    dnd: "Please, Do not disturb",
-    offline: "I'm currently offline",
-  };
+const statusMap = {
+  online: "I'm currently online",
+  idle: "Idle",
+  dnd: "Please, Do not disturb",
+  offline: "I'm currently offline",
+};
 
-  function fetchStatus(newText, status) {
-    statusText.classList.remove("animate__fadeIn");
+function fetchStatus(newText, status) {
+  statusText.classList.remove("animate__fadeIn");
+  void statusText.offsetWidth;
 
-    void statusText.offsetWidth;
+  statusText.textContent = newText;
 
-    statusText.textContent = newText;
-    statusText.classList.add("animate__animated", "animate__fadeIn");
+  statusText.classList.remove("online", "idle", "dnd", "offline");
+  statusText.classList.add(status);
 
-    statusDot.className = `dot ${status}`;
-    statusDot.classList.remove("animate__pulse");
-    
-    void statusDot.offsetWidth;
+  statusText.classList.add("animate__animated", "animate__fadeIn");
+}
 
-    statusDot.classList.add("animate__animated", "animate__pulse");
-  }
+async function updateDiscordStatus() {
+  const userId = "1245158014969974930";
 
-  async function updateDiscordStatus() {
-    const userId = "1245158014969974930";
-  
-    try {
-      const response = await fetch(`https://api.lanyard.rest/v1/users/${userId}`);
-      const { data } = await response.json();
-      const status = data.discord_status;
+  try {
+    const response = await fetch(`https://api.lanyard.rest/v1/users/${userId}`);
+    const { data } = await response.json();
+    const status = data.discord_status;
 
-      const newText = statusMap[status] || "Unknown status";
+    const newText = statusMap[status] || "Unknown status";
 
-      statusDot.className = `dot ${status}`;
-
-      if(statusText.textContent !== newText) {
-        fetchStatus(newText, status);
-      }
-
-    } catch (error) {
-      statusText.textContent = "Failed to fetch status";
+    if (statusText.textContent !== newText) {
+      fetchStatus(newText, status);
     }
+  } catch (error) {
+    statusText.textContent = "Failed to fetch status";
   }
+}
 
-  updateDiscordStatus();
-  setInterval(updateDiscordStatus, 3000);
+updateDiscordStatus();
+setInterval(updateDiscordStatus, 3000);
