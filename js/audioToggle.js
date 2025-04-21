2cd7cf003997d78de073ec975b1485b58cd3c97b
mauricioboninno <mauribonino5@gmail.com>
2025-04-21 07:15:32 -0300

    Test

--- js/audioToggle.js
+++ js/audioToggle.js
@@ -1,14 +1,11 @@
   const muteButton = document.getElementById('mute-toggle');
   const audio = document.getElementById('bg-audio');
   const icon = muteButton.querySelector('i');
-  const volumeSliderContainer = document.getElementById('volume-slider-container');
-  const volumeSlider = document.getElementById('volume-slider');
 
   const initializeAudio = () => {
     audio.currentTime = 0;
     audio.volume = 0;
     updateIcon(false);
-    volumeSlider.value = 0;
   }
 
   const updateIcon = (isUnmuted) => {
@@ -26,14 +23,6 @@
     if(isMuted) audio.play();
   
     updateIcon(!isMuted);
-    toggleVolumeMenu(isMuted);
-  
-    saveAudioState();
-  });
-
-  volumeSlider.addEventListener('input', (event) => {
-    audio.volume = parseFloat(event.target.value);
-    updateIcon(audio.volume > 0);
   });
 
   initializeAudio();
