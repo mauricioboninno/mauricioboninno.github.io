cf1c41f73a27cc514a6bb2773583f79066288e96
mauricioboninno <mauribonino5@gmail.com>
2025-04-21 12:04:51 -0300

    Test

--- js/audio.js
+++ js/audio.js
@@ -106,14 +106,19 @@
     async startAudio() {
       const { audio } = this.#elements;
       const { initialTime } = this.#state;
+
+      audio.currentTime = initialTime;
+      await audio.play();
+      this.#state.isPlaying = true;
+      this.#updateButtonIcon();
   
-      try {
+      /*try {
         audio.currentTime = initialTime;
         await audio.play();
         this.#state.isPlaying = true;
         this.#updateButtonIcon();
       } catch (error) {
         console.log(error);
-      }
+      }*/
     }
   }
