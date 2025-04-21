5b14614d41525b1e27fea24c94aa0192b4f12157
mauricioboninno <mauribonino5@gmail.com>
2025-04-21 06:22:20 -0300

    Test

--- js/audioToggle.js
+++ js/audioToggle.js
@@ -1,12 +1,20 @@
   const muteButton = document.getElementById('mute-toggle');
   const audio = document.getElementById('bg-audio');
   const icon = muteButton.querySelector('i');
+  const volumeSliderContainer = document.getElementById('volume-slider-container');
+  const volumeSlider = document.getElementById('volume-slider');
 
   audio.volume = 0;
   icon.classList.remove('fa-volume-up');
   icon.classList.add('fa-volume-mute');
 
   muteButton.addEventListener('click', () => {
+    if (volumeSliderContainer.style.display === 'none' || volumeSliderContainer.style.display === '') {
+      volumeSliderContainer.style.display = 'block';
+    } else {
+      volumeSliderContainer.style.display = 'none';
+    }
+
     if (audio.volume === 0) {
       audio.volume = 1;
       audio.play();
@@ -14,6 +22,17 @@
       icon.classList.add('fa-volume-up');
     } else {
       audio.volume = 0;
+      icon.classList.remove('fa-volume-up');
+      icon.classList.add('fa-volume-mute');
+    }
+  });
+
+  volumeSlider.addEventListener('input', (event) => {
+    audio.volume = event.target.value;
+    if (audio.volume > 0) {
+      icon.classList.remove('fa-volume-mute');
+      icon.classList.add('fa-volume-up');
+    } else {
       icon.classList.remove('fa-volume-up');
       icon.classList.add('fa-volume-mute');
     }
