a856ee7da22ce2cdbccf1a818633f9f9ec3481b8
mauricioboninno <mauribonino5@gmail.com>
2025-04-21 07:22:12 -0300

    Update audioToggle.js

--- js/audioToggle.js
+++ js/audioToggle.js
@@ -1,3 +1,4 @@
+document.addEventListener('DOMContentLoaded', () => {
   const muteButton = document.getElementById('mute-toggle');
   const audio = document.getElementById('bg-audio');
   const icon = muteButton.querySelector('i');
@@ -6,7 +7,7 @@
     audio.currentTime = 0;
     audio.volume = 0;
     updateIcon(false);
-  }
+  };
 
   const updateIcon = (isUnmuted) => {
     icon.classList.toggle('fa-volume-up', isUnmuted);
@@ -16,9 +17,10 @@
   muteButton.addEventListener('click', () => {
     const isMuted = audio.volume === 0;
     audio.volume = isMuted ? 1 : 0;
-    if(isMuted) audio.play();
-  
+    if (isMuted) audio.play();
+
     updateIcon(!isMuted);
   });
 
   initializeAudio();
+});
