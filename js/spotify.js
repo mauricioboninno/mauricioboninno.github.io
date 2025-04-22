4812947d9d4de1e5f06d2177426164307dc691da
mauricioboninno <mauribonino5@gmail.com>
2025-04-22 08:11:54 -0300

    ?

--- js/spotify.js
+++ js/spotify.js
@@ -1,5 +1,5 @@
   export class Spotify {
-  static DEFAULT_IMAGE = 'https://lastfm.freetls.fastly.net/i/u/300x300/2a96cbd8b46e442fc41c2b86b821562f.png';
+  static DEFAULT_IMAGE = 'img/defaultImage.jpg';
   static LASTFM_API_URL = 'https://ws.audioscrobbler.com/2.0/';
   static LOCALSTORAGE_KEY = 'lastPlayedTrack';
 
