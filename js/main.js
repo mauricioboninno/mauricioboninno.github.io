d50cf0dd7216c84f95254379ddb682a7b8db9707
mauricioboninno <mauribonino5@gmail.com>
2025-04-22 08:28:32 -0300

    Test

--- js/main.js
+++ js/main.js
@@ -1,16 +1,16 @@
   import { Quotes } from './quotes.js';
   import { Footer } from './footer.js';
-  import { Discord } from './discord.js';
+  import { Discord } from './app/discord.js';
+  import { Spotify } from './app/spotify.js';
   import { Audio } from './audio.js';
-  import { Spotify } from './spotify.js';
 
   function initializeApp() {
     const modules = [
         { instance: new Quotes(), method: 'startCycling' },
         { instance: new Footer(), method: 'startCycling' },
         { instance: new Discord(), method: 'startTracking' },
-        { instance: new Audio(), method: 'startAudio' },
         { instance: new Spotify(), method: 'startChecking' },
+        { instance: new Audio(), method: 'startAudio' },
     ];
 
     modules.forEach(({ instance, method }) => {
